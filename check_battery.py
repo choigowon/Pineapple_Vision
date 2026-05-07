2092b0410085b94ae3fe7f22bc6f410028845ce0
YangHeeChang <diogmlckd@naver.com>
2026-05-07 14:10:13 +0900

    파일에 맞는 함수 정의

--- check_battery.py
+++ check_battery.py
@@ -1,4 +1,5 @@
 import time
+import subprocess
 
 # 설계 시 정한 보조배터리 완충 시 최대 가동 시간 (예: 4시간 = 240분)
 MAX_OPERATING_MINUTES = 240 
@@ -17,3 +18,17 @@ def get_estimated_remaining_time():
     percent = int((remaining_minutes / MAX_OPERATING_MINUTES) * 100)
     
     return remaining_minutes, percent
+
+def check_under_voltage():
+    try:
+        # 라즈베리 파이 전용 명령어 실행
+        result = subprocess.check_output(['vcgencmd', 'get_throttled']).decode()
+        status = int(result.split('=')[1], 16)
+        
+        # 0x1: 현재 저전압 발생 중, 0x10000: 최근에 저전압 발생했었음
+        if status & 0x1:
+            return "긴급. 전압이 낮습니다. 보조배터리를 점검하세요."
+        return None
+    except:
+        # PC(Windows) 환경에서는 에러가 나므로 무시
+        return None
--- tts.py
+++ tts.py
@@ -2,20 +2,6 @@ import subprocess
 import queue
 import threading
 import time
-
-def check_under_voltage():
-    try:
-        # 라즈베리 파이 전용 명령어 실행
-        result = subprocess.check_output(['vcgencmd', 'get_throttled']).decode()
-        status = int(result.split('=')[1], 16)
-        
-        # 0x1: 현재 저전압 발생 중, 0x10000: 최근에 저전압 발생했었음
-        if status & 0x1:
-            return "긴급. 전압이 낮습니다. 보조배터리를 점검하세요."
-        return None
-    except:
-        # PC(Windows) 환경에서는 에러가 나므로 무시
-        return None
         
 class VoiceManager:
     def __init__(self):
